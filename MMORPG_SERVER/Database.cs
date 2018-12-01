40b203a6b5774a4cfe4d7555a3b6ca5c50fce663
chopx2 <chopx2@yahoo.com>
2018-12-01 00:45:15 -0500

    Initial Commit

---
+++ MMORPG_SERVER/Database.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace MMORPG_SERVER
+{
+    class Database
+    {
+           public void AddAccount(string username, string password)
+        {
+            var db = Globals.mysql.DB_RS;
+            {
+                db.Open("SELECT * FROM accounts WHERE 0=1", Globals.mysql.DB_CONN, ADODB.CursorTypeEnum.adOpenStatic, ADODB.LockTypeEnum.adLockOptimistic);
+                db.AddNew();
+                db.Fields["Username"].Value = username;
+                db.Fields["Password"].Value = password;
+                db.Update();
+                db.Close();
+            }
+
+        }
+      
+    }
+}
---
+++ MMORPG_SERVER/NetworkHandleData.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MMORPG_SERVER
+{
+    class NetworkHandleData
+    {
+        private delegate void Packet_(int Index, byte[] Data);
+        private Dictionary<int, Packet_> Packets;
+
+        public void InitMessages()
+        {
+            Packets = new Dictionary<int, Packet_>();
+            Packets.Add(1, HandleNewAccount);
+        }
+
+        public void HandleData(int index, byte[] data)
+        {
+            int packetnum;
+            Packet_ Packet;
+            KaymakGames.KaymakGames buffer = new KaymakGames.KaymakGames();
+            buffer.WriteBytes(data);
+            packetnum = buffer.ReadInteger();
+            buffer = null;
+
+            if (packetnum == 0)
+                return;
+
+            if (Packets.TryGetValue(packetnum, out Packet))
+            {
+                Packet.Invoke(index, data);
+            }
+
+        }
+
+        void HandleNewAccount(int index, byte[]data)
+        {
+            KaymakGames.KaymakGames buffer = new KaymakGames.KaymakGames();
+            buffer.WriteBytes(data);
+
+            int packetNum = buffer.ReadInteger();
+            string username = buffer.ReadString();
+            string password = buffer.ReadString();
+
+            Globals.database.AddAccount(username,password);
+
+        }
+    }
+}
